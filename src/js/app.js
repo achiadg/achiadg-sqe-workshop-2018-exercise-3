dae2426656de2de59f49ed4cffc8bd9270c47000
achiadg <achiadg@gmail.com>
2018-12-21 12:59:59 +0200

    parse arguments and code.

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
-import $ from 'jquery';
 import {parseCode} from './code-analyzer';
+import {createElementsResult} from './parser';
+import {parseArgs} from './params';
+import $ from 'jquery';
+
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
-        let codeToParse = $('#codePlaceholder').val();
+        let codeToParse = $('#inputPlaceHolder').val();
         let parsedCode = parseCode(codeToParse);
-        $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        let argsValues = parseArgs($('#inputArgs').val());
+        let elements = createElementsResult(parsedCode);
     });
 });
---
+++ src/js/params.js
@@ -0,0 +1,56 @@
+export {parseArgs};
+var i = 0;
+
+function parseArgs(valueOfAllArgs) {
+    const argsText = valueOfAllArgs.split(',');
+    let argsVals = iterateStr(argsText , []);
+    return argsVals;
+}
+
+function iterateStr(argsText , argsVals) {
+    while(i < argsText.length){
+        argsVals = checkIfCanAddToResult(argsText , argsVals);
+        argsVals = checkIfArray(argsText , argsVals);
+        argsVals = checkIfLength0(argsText , argsVals);
+    }
+    i = 0;
+    return argsVals;
+}
+
+function checkIfArray(argsText , argsVals) {
+    if(argsText[i] !== undefined){
+        if(argsText[i].charAt(0) === '[') {
+            let result = argsText[i];
+            i++;
+            while (i < argsText.length && argsText[i].charAt(argsText[i].length - 1) !== ']') {
+                result = result + ',' + argsText[i];
+                i++;
+            }
+            result = result + ',' + argsText[i];
+            argsVals.push(result);
+            i++;
+        }
+    }
+    return argsVals;
+}
+
+function checkIfLength0(argsText , argsVals) {
+    if(argsText[i] !== undefined) {
+        if (argsText[i].length === 0) {
+            argsVals.push('');
+            i++;
+        }
+    }
+    return argsVals;
+}
+
+function checkIfCanAddToResult(argsText , argsVals) {
+    if(argsText[i] !== undefined) {
+        if (argsText[i].length > 0 && argsText[i].charAt(0) !== '[') {
+            argsVals.push(argsText[i]);
+            i++;
+        }
+    }
+    return argsVals;
+}
+
